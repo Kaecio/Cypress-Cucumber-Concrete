1289af0b1ceb47cca0d2f5490a406d78fbc0d419
Henrique Braga <h.junqueira.braga@accenture.com>
2023-04-10 16:53:50 -0300

    melhora espaçamento

--- cypress/e2e/cadastrarUsuario/cadastrarUsuario.js
+++ cypress/e2e/cadastrarUsuario/cadastrarUsuario.js
@@ -1,11 +1,11 @@
 ///<reference types="cypress" />
-import {Given, When, Then} from "cypress-cucumber-preprocessor/steps"
+import { Given, When, Then } from "cypress-cucumber-preprocessor/steps"
 
 beforeEach(() => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+	cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+	cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
 	cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
-    cy.get('.oxd-button').click()
+	cy.get('.oxd-button').click()
 	cy.wait(2000)
 	cy.get(':nth-child(1) > .oxd-main-menu-item').click()
 });
@@ -18,7 +18,7 @@ Given(/^que estou na página de Admin$/, () => {
 
 When(/^clico no botão Add$/, () => {
 	cy.wait(2000)
-	cy.get('.orangehrm-header-container > .oxd-button').should('be.visible').click({force: true});
+	cy.get('.orangehrm-header-container > .oxd-button').should('be.visible').click({ force: true });
 	// cy.get('.orangehrm-header-container > .oxd-button')
 	cy.wait(2000)
 });
@@ -29,8 +29,9 @@ When(/^submeto o seguinte formulário de cadastro "([^"]*)"$/, (nome_do_empregad
 	cy.wait(2000)
 	cy.get('.oxd-autocomplete-option').first().click()
 	cy.wait(500)
-	
+
 });
+
 
 When(/^clico no botão Save$/, () => {
 	cy.get('.oxd-button--secondary').click()
@@ -38,17 +39,16 @@ When(/^clico no botão Save$/, () => {
 
 
 Then(/^vejo a mensagem de "([^"]*)" nos campos não preenchidos$/, (message) => {
-	
-	cy.get(':nth-child(1) > .oxd-grid-2 > :nth-child(1) > .oxd-input-group > .oxd-text').should('have.text',message)
-	cy.get(':nth-child(4) > .oxd-input-group > .oxd-text').should('have.text',message)
-	cy.get(':nth-child(3) > .oxd-input-group > .oxd-text').should('have.text',message)
-	cy.get('.user-password-cell > .oxd-input-group > .oxd-text').should('have.text',message)
-	cy.get(':nth-child(4) > .oxd-input-group > .oxd-text').should('have.text',message)
-	cy.get(':nth-child(2) > .oxd-input-group > .oxd-text').should('have.text',message)
+	cy.get(':nth-child(1) > .oxd-grid-2 > :nth-child(1) > .oxd-input-group > .oxd-text').should('have.text', message)
+	cy.get(':nth-child(4) > .oxd-input-group > .oxd-text').should('have.text', message)
+	cy.get(':nth-child(3) > .oxd-input-group > .oxd-text').should('have.text', message)
+	cy.get('.user-password-cell > .oxd-input-group > .oxd-text').should('have.text', message)
+	cy.get(':nth-child(4) > .oxd-input-group > .oxd-text').should('have.text', message)
+	cy.get(':nth-child(2) > .oxd-input-group > .oxd-text').should('have.text', message)
 });
 
 
-When(/^preencho os seguites dados corretamente "([^"]*)" "([^"]*)" "([^"]*)" "([^"]*)" "([^"]*)"$/, (user_role,employe_name,status,username,password) => {
+When(/^preencho os seguites dados corretamente "([^"]*)" "([^"]*)" "([^"]*)" "([^"]*)" "([^"]*)"$/, (user_role, employe_name, status, username, password) => {
 	cy.get(':nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text').click()
 	cy.wait(1000)
 	cy.contains('.oxd-select-dropdown > *', user_role).click();
@@ -66,18 +66,14 @@ When(/^preencho os seguites dados corretamente "([^"]*)" "([^"]*)" "([^"]*)" "([
 	cy.wait(2000)
 	cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type(password)
 	cy.wait(2000)
-
 });
 
 
 Then(/^vejo o pop up com a seguinte mensagem "([^"]*)"$/, (message) => {
-	// cy.wait(4500)
 	cy.get('.oxd-text--toast-title').should('have.text', message)
 });
 
 
 When(/^vejo o novo usuário cadastrado em tela$/, () => {
-	
+
 });
-
-
--- cypress/e2e/login/login.js
+++ cypress/e2e/login/login.js
@@ -1,50 +1,56 @@
 ///<reference types="cypress" />
-import {Given, When, Then} from "cypress-cucumber-preprocessor/steps"
+import { Given, When, Then } from "cypress-cucumber-preprocessor/steps"
 
 Given(/^que eu estou na página de login$/, () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 });
 
+
 When(/^preencher somente o campo Password$/, () => {
-	cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
 });
+
 
 When(/^clicar no botão Login$/, () => {
-	cy.get('.oxd-button').click()
+    cy.get('.oxd-button').click()
 });
+
 
 Then(/^eu vejo a alert "([^"]*)" no input Username$/, (message) => {
-    cy.get('.oxd-input-group > .oxd-text').should('have.text',message)
+    cy.get('.oxd-input-group > .oxd-text').should('have.text', message)
 });
 
+
 When(/^preencher somente o campo Username$/, () => {
-	cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Kaecio')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Kaecio')
 });
 
 
 Then(/^eu vejo a alert "([^"]*)" no input Password$/, (message) => {
-	cy.get(':nth-child(3) > .oxd-input-group > .oxd-text').should('have.text',message )
+    cy.get(':nth-child(3) > .oxd-input-group > .oxd-text').should('have.text', message)
 });
 
 
 When(/^eu não preencher os campos Username e Password$/, () => {
-	cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('{selectall}{backspace}')
-	cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('{selectall}{backspace}')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('{selectall}{backspace}')
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('{selectall}{backspace}')
 });
 
 
 Then(/^eu vejo a alert "([^"]*)" no input Username e no input Password$/, (message) => {
-    cy.get(':nth-child(2) > .oxd-input-group > .oxd-text').should('have.text',message)
-    cy.get(':nth-child(3) > .oxd-input-group > .oxd-text').should('have.text',message )
+    cy.get(':nth-child(2) > .oxd-input-group > .oxd-text').should('have.text', message)
+    cy.get(':nth-child(3) > .oxd-input-group > .oxd-text').should('have.text', message)
 });
+
 
 When(/^preencho os campos Username e Password corretamente$/, () => {
-	cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
-	cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
 });
 
+
 Then(/^sou direcionado para a página Home$/, () => {
-	cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').should('be.visible').should('have.text', 'Dashboard')
+    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').should('be.visible').should('have.text', 'Dashboard')
     cy.location().should((location) => {
         expect(location.pathname).to.eq('/web/index.php/dashboard/index')
         expect(location.host).to.eq('opensource-demo.orangehrmlive.com')
