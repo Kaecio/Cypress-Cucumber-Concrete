dbba787c076ae24863ab56ead32eb0dfa596bedf
-g <kaecio>
2023-04-14 11:50:36 -0300

    Alterando codigo

--- cypress/e2e/cadastrarUsuario/cadastrarUsuario.js
+++ cypress/e2e/cadastrarUsuario/cadastrarUsuario.js
@@ -1,4 +1,5 @@
 ///<reference types="cypress" />
+import { should } from "chai";
 import { Given, When, Then } from "cypress-cucumber-preprocessor/steps"
 
 beforeEach(() => {
@@ -6,9 +7,7 @@ beforeEach(() => {
 	cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
 	cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
 	cy.get('.oxd-button').click()
-	cy.wait(2000)
-	cy.get(':nth-child(1) > .oxd-main-menu-item').click()
-	cy.wait(5000)	
+	cy.get(':nth-child(1) > .oxd-main-menu-item',{timeout:4000}).should('be.visible').click()
 });
 
 Given(/^que estou na página de Admin$/, () => {
@@ -17,17 +16,15 @@ Given(/^que estou na página de Admin$/, () => {
 
 
 When(/^adiciono um novo usuário$/, () => {
-	cy.wait(2000)
-	cy.get('.orangehrm-header-container > .oxd-button').should('be.visible').click({ force: true });
+	cy.get('.orangehrm-header-container > .oxd-button',{timeout: 3000}).should('be.visible').click({ force: true });
 	// cy.get('.orangehrm-header-container > .oxd-button')
-	cy.wait(2000)
+
 });
 
 When(/^submeto o seguinte formulário de cadastro "([^"]*)"$/, (nome_do_empregado) => {
 	cy.get('.oxd-autocomplete-text-input > input').type(nome_do_empregado)
-	cy.wait(2000)
-	cy.get('.oxd-autocomplete-option').first().click()
-	cy.wait(500)
+	cy.wait(3000)
+	cy.get('.oxd-autocomplete-option',{timeout: 3000}).should('be.visible').first().click()
 
 });
 
@@ -55,14 +52,10 @@ When(/^preencho os seguites dados corretamente "([^"]*)" "([^"]*)" "([^"]*)" "([
 	cy.wait(3000)
 	cy.get('.oxd-autocomplete-option').first().click()
 	cy.wait(500)
-	cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text').click();
-	cy.wait(1000)
-	cy.contains('.oxd-select-dropdown > *', status).click();
-	cy.wait(1000)
-	cy.get(':nth-child(4) > .oxd-input-group > :nth-child(2) > .oxd-input').type(username)
-	cy.wait(3000)
-	cy.get('.user-password-cell > .oxd-input-group > :nth-child(2) > .oxd-input').type(password)
-	cy.wait(2000)
+	cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text',{timeout: 2000}).should('be.visible').click();
+	cy.contains('.oxd-select-dropdown > *', status,{status:2000}).should('be.visible').click();
+	cy.get(':nth-child(4) > .oxd-input-group > :nth-child(2) > .oxd-input',{timeout:5000}).should('be.visible').type(username)
+	cy.get('.user-password-cell > .oxd-input-group > :nth-child(2) > .oxd-input',{timeout:4000}).should('be.visible').type(password)
 	cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type(password)
 	cy.wait(2000)
 });
